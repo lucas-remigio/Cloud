b386db858533d645315eaae7eb24373fa1895a44
Lucas Remígio <lucasxavier5435@hotmail.com>
2025-04-22 23:10:16 +0100

    fix: envs

--- frontend/src/app/connections.ts
+++ frontend/src/app/connections.ts
@@ -1,11 +1,11 @@
 export const backend_port = process.env.BACKEND_PORT || 3000;
 export const websocket_port = process.env.WEBSOCKET_PORT || 3002;
 
-const backend = process.env.BACKEND_URL || "http://localhost";
-const websocket = process.env.WEBSOCKET_URL || "ws://localhost";
+const backend = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost";
+const websocket = process.env.NEXT_PUBLIC_WEBSOCKET_URL || "ws://localhost";
 
-console.log("Backend Process URL:", process.env.BACKEND_URL);
-console.log("WebSocket Process URL:", process.env.WEBSOCKET_URL);
+console.log("Backend Process URL:", process.env.NEXT_PUBLIC_BACKEND_URL);
+console.log("WebSocket Process URL:", process.env.NEXT_PUBLIC_WEBSOCKET_URL);
 console.log("Backend URL:", backend);
 console.log("WebSocket URL:", websocket);
 

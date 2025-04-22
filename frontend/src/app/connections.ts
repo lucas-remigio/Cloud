edaf4fdfc750173c1f89aed9badee179502c5e2b
Lucas Remígio <lucasxavier5435@hotmail.com>
2025-04-22 22:22:01 +0100

    add: debug

--- frontend/src/app/connections.ts
+++ frontend/src/app/connections.ts
@@ -4,5 +4,8 @@ export const websocket_port = process.env.WEBSOCKET_PORT || 3002;
 const backend = process.env.BACKEND_URL || "http://localhost";
 const websocket = process.env.WEBSOCKET_URL || "ws://localhost";
 
+console.log("Backend URL:", backend);
+console.log("WebSocket URL:", websocket);
+
 export const backend_url = `${backend}:${backend_port}`;
 export const websocket_url = `${websocket}:${websocket_port}`;
